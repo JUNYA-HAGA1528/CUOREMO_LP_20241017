916e5471ad1bb99bed79bd771e28eb24d30ec0b9
hagajunya <n0812034@yahoo.co.jp>
2024-10-24 12:45:39 +0900

    2024/10/24

--- src/js/script.js
+++ src/js/script.js
@@ -20,21 +20,19 @@ document.querySelectorAll('.accordion__header').forEach(button => {
 document.addEventListener("scroll", function () {
     const doctorButton = document.querySelector(".doctor__buttons");
     const ctaButtons = document.querySelector(".cta__buttons");
-    const fagButtons = document.querySelector(".jp-access"); // または footer
 
-    if (doctorButton && fagButtons) {
+    if (doctorButton) {
         const doctorButtonBottom = doctorButton.getBoundingClientRect().bottom;
-        const fagButtonsTop = fagButtons.getBoundingClientRect().top; // または footer
-        const windowHeight = window.innerHeight;
 
         // doctor__buttons が画面の上に完全に消えたらCTAボタンを表示
-        if (doctorButtonBottom < 0 && fagButtonsTop > windowHeight) {
+        if (doctorButtonBottom < 0) {
             ctaButtons.classList.add("show");  // クラス追加でCTAボタンを表示
         } else {
             ctaButtons.classList.remove("show"); // クラス削除でCTAボタンを非表示
         }
     }
 });
+
 
 
 
